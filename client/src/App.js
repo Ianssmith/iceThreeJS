404f83ae35bd738787be3a9f7f1b4bf0f0374d3f
ianssmith <ian.s.smith0317@gmail.com>
2020-03-12 14:45:28 -0600

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ThreeContainer from "./components/ThreeContainer";
+import NavBar from "./components/NavBar";
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <ThreeContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Sign Up</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
